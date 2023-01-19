b7108c9c57066d87d3db916a954780b7a8c8720f
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-19 15:55:40 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -9,10 +9,7 @@ import koreanize_matplotlib
 st.header('연습용 형태소 분석기입니다 ')
 corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
 
-mecab = MeCab(user_dictionary_path="nouns.dic")
-mecab.dictionary
-
-#mecab = MeCab()
+mecab = MeCab()
 
 me1 = mecab.morphs(corpus)
 me2 = mecab.nouns(corpus)
