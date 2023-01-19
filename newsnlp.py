15f47f689aa651d2d426039c53a6a21d7599e309
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-19 10:32:14 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -2,12 +2,11 @@ import requests
 import matplotlib.pyplot as plt
 from konlpy.tag import Kkma
 from konlpy.tag import Okt
-from konlpy.tag import Mecab
 from wordcloud import WordCloud
 import streamlit as st
 import koreanize_matplotlib
 
-mecab = Mecab()
+okt = Okt()
 
 def ko_preprocess_sentence(sentence, s_token=False, e_token=False):
     sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
@@ -15,7 +14,7 @@ def ko_preprocess_sentence(sentence, s_token=False, e_token=False):
     sentence = re.sub(r"[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z?.!,]+", " ", sentence)
 
     sentence = sentence.strip()
-    sentence = mecab.morphs(sentence)
+    sentence = okt.morphs(sentence)
 
     if s_token:
         sentence = '<start> ' + sentence
@@ -28,14 +27,12 @@ def ko_preprocess_sentence(sentence, s_token=False, e_token=False):
 
 st.header('연습용 형태소 분석기입니다 ')
 
-kor_raw_corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
-eng_raw_corpus = st.text_input('영어입력', 'What is your name?')
+corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
 
-for kor, eng in cleaned_corpus:
-    kor_raw_corpus.append(ko_preprocess_sentence(kor))
-    eng_raw_corpus.append(en_preprocess_sentence(eng,s_token=True, e_token=True))
+for kor in cleaned_corpus:
+    corpus.append(ko_preprocess_sentence(kor))
+    
 
-st.table(kor_raw_corpus)
-st.table(eng_raw_corpus)
+st.table(corpus)
 
 
