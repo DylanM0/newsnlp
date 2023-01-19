d3e17e11808a52413111bf4cf1d7999760ca0093
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-19 15:52:29 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -4,10 +4,16 @@ from wordcloud import WordCloud
 import streamlit as st
 import koreanize_matplotlib
 
+$ python3 -m mecab dict-index --userdic nouns.dic nouns.csv
+
+
 st.header('연습용 형태소 분석기입니다 ')
 corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
 
-mecab = MeCab()
+mecab = MeCab(user_dictionary_path="nouns.dic")
+mecab.dictionary
+
+'mecab = MeCab()
 
 me1 = mecab.morphs(corpus)
 me2 = mecab.nouns(corpus)
