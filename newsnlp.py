6b038c692e89c92ad686c2a2207da5d7aa4f73ee
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-19 10:53:56 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -4,37 +4,20 @@ from mecab import MeCab
 from wordcloud import WordCloud
 import streamlit as st
 import koreanize_matplotlib
-import re
 
+st.header('연습용 형태소 분석기입니다 ')
+corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
 
 mecab = MeCab()
 
-def ko_preprocess_sentence(sentence, s_token=False, e_token=False):
-    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-    sentence = re.sub(r'[" "]+', " ", sentence)
-    sentence = re.sub(r"[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z?.!,]+", " ", sentence)
-
-    sentence = sentence.strip()
-    sentence = mecab.morphs(sentence)
-
-    if s_token:
-        sentence = '<start> ' + sentence
-
-    if e_token:
-        sentence += ' <end>'
-    
-    return sentence
+me1 = mecab.morphs(corpus)
+me2 = mecab.nouns(corpus)
+me3 = mecab.pos(corpus)
 
 
-st.header('연습용 형태소 분석기입니다 ')
 
-corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
-
-corpus1 =[]
-for kor in corpus:
-    corpus1.append(ko_preprocess_sentence(kor))
-    
-
-st.table(corpus1)
+st.table(me1)
+st.table(me2)
+st.table(me3)
 
 
