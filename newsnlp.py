e6065f92ca832d821baf9d1c4b72a79f9ea799de
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-21 15:50:03 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -88,12 +88,12 @@ if uploaded_file:
 
     choice_column = st.selectbox('선택해주세요',choice, )
     
-    options = st.multiselect('선택해주세요',choice1)
+    options = st.selectbox('선택해주세요',choice1)
 
 
-    명사카운트 = 합격[합격['모집단위'] == choice_column]
+    명사카운트 = 합격[(합격['모집단위'] == choice_column)&(합격['편제'] == options)]
 
-    명사카운트1 = 합격[합격['편제'] == options]
+  
 
 
 
