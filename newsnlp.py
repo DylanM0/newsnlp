33a0e4ee7d4af7d1e89c3792d8efa5b1674f86e6
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-21 14:41:43 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -67,12 +67,13 @@ if uploaded_file:
     
     generate_excel_download_link(df)
     
+    global df
 
-df11 = dataframe(df)    
+ 
 
 
 
-choice = df11['모집단위'].unique()
+choice = df['모집단위'].unique()
     
     
 choice_column = st.selectbox('선택해주세요',choice, )
