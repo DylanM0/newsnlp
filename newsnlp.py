7538fcf5cf240cbed8613888b41e97030ba326aa
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-21 11:01:58 +0900

    Update newsnlp.py

--- newsnlp.py
+++ newsnlp.py
@@ -5,6 +5,16 @@ import streamlit as st
 import koreanize_matplotlib
 
 
+def generate_excel_download_link(df):
+    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+    towrite = BytesIO()
+    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    towrite.seek(0)  # reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+
 
 st.header('연습용 형태소 분석기입니다 ')
 corpus = st.text_input('한글입력', '당신의 이름은 무엇입니까?')
@@ -25,5 +35,17 @@ st.table(me2)
 
 st.subheader('형태소분석_2(품사포함)')
 st.table(me3)
+
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+    
+    
+    
+    
+
 
 
